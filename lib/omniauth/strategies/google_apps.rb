0eb7c192fbff8f5822bcff96dd648c56ef51cb50
Nick Snyder <nickolus451@gmail.com>
2012-12-28 10:04:43 -0600

    forcing option to choose between two different domains

--- lib/omniauth/strategies/google_apps.rb
+++ lib/omniauth/strategies/google_apps.rb
@@ -10,7 +10,7 @@ module OmniAuth
       def get_identifier
         f = OmniAuth::Form.new(:title => 'Google Apps Authentication')
         f.label_field('Google Apps Domain', 'domain')
-        f.input_field('url', 'domain')
+        f.select('domain', 'domain', options_for_select([['Dedo', 'dedoinc.com'], ['Miller & Associates', 'mmillerassociates.com']]))
         f.to_response
       end
 
